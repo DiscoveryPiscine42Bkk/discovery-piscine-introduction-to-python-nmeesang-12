77e36fc3848e21856580c2275e93dfd2346e9b49
nmeesang-12 <p.ntcpms@gmail.com>
2025-06-19 15:06:51 +0000

    add

---
+++ cell05/ex01/play_with_array.py
@@ -0,0 +1,12 @@
+def main():
+    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    new_array = []
+
+    for num in original_array:
+        new_array.append(num + 2)
+
+    print("Original array:", original_array)    
+    print("New array:", new_array)
+
+if __name__ == "__main__"  
+    main()
