b86fac8374c21cc9b695df5c5bb9b3b9c2d7df1c
nmeesang-12 <p.ntcpms@gmail.com>
2025-06-20 04:07:19 +0000

    add

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,14 @@
+import sys
+import reversed
+
+if len(sys.argv) != 3:
+    print("none")
+else:
+    keyword + sys.argv[1]
+    text = sys.argv[2]
+    matches + re.findall(rf'\b{re.escape(keyword)}\b', text)
+    if matches:
+        print(len(matches))
+    else:
+        print("none")
+
